f9b2b25a3a94997cfd8fc619c4d3cc204e93ca5d
Lucas J Santos <santos.j.lucas@gmail.com>
2019-04-06 15:02:39 -0300

    Adicionar Swipe

--- init.el
+++ init.el
@@ -63,3 +63,18 @@
   (setq ivy-count-format "%d/%d ")
   (setq ivy-display-style 'fancy))
 
+; Swipe
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper)
+	 ("C-c C-r" . ivy-resume)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
