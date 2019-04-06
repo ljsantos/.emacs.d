748555225a7c9cab49451ed1d94307a72d5eca2b
Lucas J Santos <santos.j.lucas@gmail.com>
2019-04-06 18:04:37 -0300

    Config Jedi on init.el

--- init.el
+++ init.el
@@ -129,6 +129,8 @@
   :ensure t
   :config
   (add-hook 'python-mode-hook 'jedi:setup)
+  (setq jedi:setup-keys t)
+  (setq jedi:complete-on-dot t)
   )
 
 (defun my/python-mode-hook ()
@@ -284,3 +286,17 @@ Git gutter:
               (sit-for 0.1)
               (git-gutter:clear))
    :color blue))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (git-timemachine git-gutter hydra expand-region multiple-cursors hungry-delete magit yasnippet-snippets virtualenvwrapper use-package-ensure-system-package try swiper-helm powerline ivy-explorer elpy doom-themes counsel company-jedi beacon auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
