c3d5c67507e0774a796cbefd423c0ba981818427
Lucas J Santos <santos.j.lucas@gmail.com>
2019-04-07 14:33:01 -0300

    Dumb-jump

--- init.el
+++ init.el
@@ -325,3 +325,16 @@ Git gutter:
   :ensure t
   )
 (require 'wgrep-ag)
+
+; Dumb-jump
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config 
+  ;; (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+  :init
+  (dumb-jump-mode)
+  :ensure
+  )
